8f3b10a99ab52f1631cd35569ab9906dfaec06ad
Timon Ther <email.ther@gmail.com>
2019-06-07 01:15:47 +0100

    Create model_extapi.go
    simulates dialogue with external API

---
+++ model_extapi.go
@@ -0,0 +1,13 @@
+package main
+
+func extAPIgetValue(isin string) float32 {
+	// NOT YET IMPLEMENTED
+	return 42.1
+}
+
+func productInfoByISIN(isin string) (Product, error) {
+	// NOT YET IMPLEMENTED
+	ex1 := Product{"Amundi Test", "AT", "FR01234556", 10.05, 23.45, 213.5, 1.1}
+
+	return ex1, nil
+}
